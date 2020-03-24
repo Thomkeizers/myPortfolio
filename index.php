a07c53daf17ea34d7f93bb041f2ec7c85f994e59
Thomkeizers <t.keizers@live.nl>
2020-03-24 13:46:17 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__center">
+            <h1 class="heading-primary">
+                Thom Keizers<br>
+                <img class="header__img" src="img/pf.jpg" alt="Author Profile Picture"><br>
+            </h1>
+        <div class="center-horizontal">
+            <a class="header__button btn btn--white" href="#">Show portfolio &rarr;</a>
+        </div>
+        
+        </div>
+        
+    </header>
+
+    <section class="section-about margin-bottom-big ">
+        <h2 class="heading-secondary margin-top-big">
+            About this website
+        </h2>
+
+        <div class="row ">
+            <div class="col-1-of-2 margin-top-ultimate">
+                <div class="section-about__aboutdiv">
+                    <h3 class="heading-tertiary">Personal info</h3>
+                    <p class="heading-tertiary__aboutpar">Mijn naam is Thom Keizers. Ik ben 25 jaar oud en kom uit Horst. Ik ben een student aan ROC ter AA in Helmond. Hier volg ik de opleiding tot applicatie ontwikkelaar. Ik ben momenteel een 1e-jaarsstudent.
+                        Ik ben momenteel het liefst bezig met web development. Zowel back- als front end vind ik leuk. Ook is applicatie programmeren een van mijn favoriete vakken. Ik ben erg leergierig en sta open nieuwe technieken te leren.
+                        Ik wil later eventueel een eigen webbureau starten. In mijn aankomende stages wil ik graag uitgedaagd worden. Denk aan het leren van PHP-frameworks als Symphony en Laravel. Aan de andere kant wil ik advanced CSS leren d.m.v. SASS.
+                    </p>
+                </div>                
+            </div>
+
+            <div class="col-1-of-2 margin-top-ultimate">
+                <div class="section-about__aboutdiv">
+                    <h3 class="heading-tertiary">Project info</h3>
+                    <p class="heading-tertiary__aboutpar">Welkom in mijn portfolio. In dit project wil ik je laten zien wat ik tot nu toe allemaal geleerd heb. Ook kun je zien wat ik allemaal gemaakt heb. Je kunt verschillende projecten van mij bekijken. Zo kun je op deze website projecten van web-development, applicatie programmeren, 
+                        EBSY etc. terug vinden. Ik nodig je uit een kijkje te nemen in mijn voortgang als 1e jaars applicatie programmeur aan het ROC ter AA in Helmond. 
+                    </p>
+                </div>
+            </div>   
+        </div>
+    
+    </section>
+    
+</body>
+</html>
